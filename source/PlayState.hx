b02e839ba95ae8f6d046912479642ad3ac5b204b
Idan Kimel <idan@slatescience.com>
2018-05-08 12:51:45 +0300

    Added Amir's temporary images and implemented usage of them on info and language change For now, no info appear animation, and info doesn't block input etc... until these issues will be decided in tomorrow's meeting Also, placed correctly the illusion active/inactive button, still not interactive, will be following the meeting tomorrow

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -11,7 +11,8 @@ import openfl.Lib;
 typedef Language = {
     id : String,
     buttonSpritesheet : String,
-    backgroundImage: String
+    backgroundImage: String,
+    infoImage : String
 }
 
 class PlayState extends FlxState
@@ -21,30 +22,59 @@ class PlayState extends FlxState
     private static var BUTTON_WIDTH : Int = 83;
     private static var BUTTON_HEIGHT : Int = 83;
 
+    private static var ILLUSION_ACTIVE_DISPLAY_X : Float = 1521;
+    private static var ILLUSION_ACTIVE_DISPLAY_Y : Float = 347;
+
     private static var FIRST_LANGUAGE_X : Float =  1650;
     private static var LANGUAGE_Y : Float = 1020;
     private static var LANGUAGE_SPACING : Float = 0;
 
     private static var LANGUAGES: Array<Language> = [
-        {id: "arabic", buttonSpritesheet: "infoSpritesheet", backgroundImage: "background"},
-        {id: "english", buttonSpritesheet: "infoSpritesheet", backgroundImage: "background"},
-        {id: "hebrew", buttonSpritesheet: "infoSpritesheet", backgroundImage: "background"}
+        {id: "arabic", buttonSpritesheet: "arabicSpritesheet", backgroundImage: "backgroundArabic", infoImage: "infoArabic"},
+        {id: "english", buttonSpritesheet: "englishSpritesheet", backgroundImage: "backgroundEnglish", infoImage: "infoEnglish"},
+        {id: "hebrew", buttonSpritesheet: "hebrewSpritesheet", backgroundImage: "backgroundHebrew", infoImage: "infoHebrew"}
     ];
+
+    private static var START_LANGAUGE_INDEX : Int = 2;
 
     private var sliders : Array<Slider>;
     private var selectedLanguage : Language;
     private var background : FlxSprite;
+    private var info : FlxSprite;
+
+    private var illusionActive : FlxSprite;
+    private var illusionInactive : FlxSprite;
+
+    private var isIllusionActive : Bool;
 
     private var dotsIllusion : DotsIllusion;
 
     override public function create() : Void {
         super.create();
 
-        this.selectedLanguage = LANGUAGES[0];
+        this.selectedLanguage = LANGUAGES[START_LANGAUGE_INDEX];
 
         this.background = new FlxSprite();
+        this.info = new FlxSprite();
+        this.info.visible = false;
         this.loadLanguage();
-        add(background);
+        add(this.background);
+
+        this.illusionActive = new FlxSprite();
+        this.illusionActive.loadGraphic("assets/images/illusionActive.png");
+        this.illusionActive.x = ILLUSION_ACTIVE_DISPLAY_X;
+        this.illusionActive.y = ILLUSION_ACTIVE_DISPLAY_Y;
+        this.illusionActive.visible = false;
+        add(this.illusionActive);
+
+        this.illusionInactive = new FlxSprite();
+        this.illusionInactive.loadGraphic("assets/images/illusionNotActive.png");
+        this.illusionInactive.x = ILLUSION_ACTIVE_DISPLAY_X;
+        this.illusionInactive.y = ILLUSION_ACTIVE_DISPLAY_Y;
+        this.illusionInactive.visible = false;
+        add(this.illusionInactive);
+
+        this.setIllusionActive(true);
 
         var infoButton = new FlxButton(INFO_X - BUTTON_WIDTH / 2, INFO_Y - BUTTON_HEIGHT / 2, "", toggleInfo);
         infoButton.loadGraphic("assets/images/infoSpritesheet.png", true, BUTTON_WIDTH, BUTTON_HEIGHT);
@@ -58,6 +88,8 @@ class PlayState extends FlxState
         sliders = new Array<Slider>();
         sliders.push(new Slider(this, "slider1", new FlxRect(1733, 471, 100, 377), this.sliderChanged, 1, 30, 1, 5, 5));
         sliders.push(new Slider(this, "slider2", new FlxRect(1525, 471, 100, 377), this.sliderChanged, -5, 40, 1, 12, 12));
+
+        add(this.info);
     }
 
     override public function onFocusLost() : Void {
@@ -74,7 +106,8 @@ class PlayState extends FlxState
     }
 
     private function loadLanguage() {
-        background.loadGraphic("assets/images/" + this.selectedLanguage.backgroundImage + ".png");
+        this.background.loadGraphic("assets/images/" + this.selectedLanguage.backgroundImage + ".png");
+        this.info.loadGraphic("assets/images/" + this.selectedLanguage.infoImage + ".png");
     }
 
     private function createLanguageButtons() {
@@ -91,7 +124,17 @@ class PlayState extends FlxState
     }
 
     private function toggleInfo() : Void {
+    	this.info.visible = !this.info.visible;
+    }
 
+    private function setIllusionActive(isIllusionActive : Bool) : Void {
+    	this.isIllusionActive = isIllusionActive;
+    	this.updateIllusionActiveDisplay();
+    }
+
+    private function updateIllusionActiveDisplay() : Void {
+    	this.illusionActive.visible = this.isIllusionActive;
+    	this.illusionInactive.visible = !this.isIllusionActive;
     }
 
     private function toggleLanguage(language : Language) : Void {
